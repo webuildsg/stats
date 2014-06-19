dd4b78778ba38bba7e884a1b92604b75c24bb225
Chinmay Pendharkar <notthetup@gmail.com>
2014-06-19 22:25:03 +0800

    made static

--- app/main.js
+++ app/main.js
@@ -5,44 +5,21 @@
  * Uses Zepto.js, Handlebars.js, and d3.js
  */
 
-var log;
+ var log;
+ var length;
+ var name = "data/audio.live.webuild.sg-May-2014";
 
-/**
- * Display file input for those who don't want to use drag and drop
- */
-function showFileInput() {
-  $('#fileinput').css('display', 'block');
-  return false;
-}
+ var xhr = $.ajax({
+  type: "HEAD",
+  url: "data/audio.live.webuild.sg-May-2014",
+  success: function(msg){
+    length = xhr.getResponseHeader('Content-Length');
+    $.get( name, handleFileLoad);
+  }
+});
 
-/**
- * Handle the drop event. Stops the browser from just loading the file's contents 
- * and instead allows us to process the data. Also mimcs the system's copy action, 
- * which usually changes the cursor to indicate that it's a copy.
- *
- * @param  evt  The drag over event that triggered the event listener
- */
-function handleDragOver(evt) {
-  // Stop further propagation in DOM and cancel the event
-  evt.stopPropagation();
-  evt.preventDefault();
-  evt.dataTransfer.dropEffect = 'copy';
 
-  return false;
-}
-
-/**
- * Main function for the script. Reads the file and validates its first couple 
- * lines. If valid, it splits the file and populates the logTable. It then calls 
- * the functions necessary to organize the entries and add the charts and tables 
- * to the page.
- *
- * @param  evt  The drop or change event that triggered the event listener
- */
-function handleFileSelect(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-
+ function handleFileLoad(data) {
   // Log current time
   var startTime = new Date().getTime();
 
@@ -51,66 +28,42 @@ function handleFileSelect(evt) {
   var template = Handlebars.compile(source);
   var uploadbox = $('#uploadbox');
 
-  // Check that we have access to the necessary APIs
-  if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
-    uploadbox.html(template({ 'browserIncompatible': true }));
-    return false;
-  }
-
   // Get the uploaded file
-  var files = $('#fileinput').attr('files');
-  var file;
-
-  if (files.length)
-    file = files[0];
-  else
-    file = evt.dataTransfer.files[0];
 
   // Display File Info, replacing previous upload box contents
-  var fileInfo = { 'fileName': escape(file.name), 'fileSize': file.size };
+  var fileInfo = { 'fileName': escape(name), 'fileSize': length };
   uploadbox.html(template(fileInfo));
 
-  // Create new FileReader, and handle when done reading
-  var reader = new FileReader();
+  log = new Log(data);
 
-  reader.onloadend = function(evt) {
-    if (evt.target.readyState == FileReader.DONE) {
-      log = new Log(evt.target.result);
+  // Display error if the log file is invalid, then return and refresh
+  if (!log.isValid()) {
+    fileInfo['fileError'] = true;
+    uploadbox.html(template(fileInfo));
+    setTimeout('location.reload(true);', 4000);
+    return;
+  }
 
-      // Display error if the log file is invalid, then return and refresh
-      if (!log.isValid()) {
-        fileInfo['fileError'] = true;
-        uploadbox.html(template(fileInfo));
-        setTimeout('location.reload(true);', 4000);
-        return;
-      }
+  // Parse the log and update the page
+  log.parse();
+  setupPage();
 
-      // Parse the log and update the page
-      log.parse();
-      setupPage();
+  // Done - let's show how long that took
+  var endTime = new Date().getTime();
+  var duration = (endTime - startTime) / 1000;
 
-      // Done - let's show how long that took
-      var endTime = new Date().getTime();
-      var duration = (endTime - startTime) / 1000;
-
-      var timeTemplate = Handlebars.compile($('#time-template').html());
-      $('#footer').html(timeTemplate({ 'duration': duration }));
-    }
-  };
-
-  reader.readAsText(file);
-
-  return false;
+  var timeTemplate = Handlebars.compile($('#time-template').html());
+  $('#footer').html(timeTemplate({ 'duration': duration }));
 }
 
 /**
- * Creates an array of objects from a two dimensional array, for use with 
+ * Creates an array of objects from a two dimensional array, for use with
  * Handlebars. Adds the rank to each row.
  *
  * @param   array  Two dimensional array to format
  * @return  array  An array of objects with properties: rank, name and value
  */
-function formatTableRows(array) {
+ function formatTableRows(array) {
   var rows = [];
   for (i = 0; i < array.length; i++) {
     var row = array[i];
@@ -123,7 +76,7 @@ function formatTableRows(array) {
 /**
  * Adds the generated tables and bar charts to the main page.
  */
-function setupPage() {
+ function setupPage() {
   var div;
   var tableHtml;
 
@@ -137,12 +90,12 @@ function setupPage() {
   // Next all the barcharts and tables
   // Format: div id, table var, second column head, third column head
   var sectionInfo = [
-    ['hosts',       log.hosts,       'Host',      'Hits'],
-    ['requests',    log.requests,    'Request',   'Hits'],
-    ['pages',       log.pages,       'Page',      'Hits'],
-    ['ref',         log.referrers,   'Referrer',  'Hits'],
-    ['refdomains',  log.refDomains,  'Domain',    'Hits'],
-    ['errors',      log.errors,      'Error',    'Hits']
+  //['hosts',       log.hosts,       'Host',      'Hits'],
+  ['requests',    log.requests,    'Request',   'Hits'],
+  ['pages',       log.pages,       'Page',      'Hits'],
+  //['ref',         log.referrers,   'Referrer',  'Hits'],
+  //['refdomains',  log.refDomains,  'Domain',    'Hits'],
+  //['errors',      log.errors,      'Error',    'Hits']
   ];
 
   // Loop through each barChartInfo element
@@ -176,8 +129,8 @@ function setupPage() {
 
     // Get list of table rows to add event listeners to
     var links = div.find('.container')[0]
-                   .getElementsByClassName('table')[0]
-                   .getElementsByTagName('tr');
+    .getElementsByClassName('table')[0]
+    .getElementsByTagName('tr');
 
     // Add event listeners, but skip the row containing the table head
     for (j = 1; j < links.length; j++) {
@@ -191,7 +144,7 @@ function setupPage() {
  *
  * @param  evt  The click event that triggered the listener
  */
-function processOverlay(evt) {
+ function processOverlay(evt) {
   var query = this.getElementsByTagName('td')[1].innerHTML;
 
   // Get section id, ie: <div id="hosts" class="section">
@@ -222,7 +175,7 @@ function processOverlay(evt) {
     Charts.drawLineChart('#modal', log.parseTraffic('host', query));
   }
 
-  // Render data for all other sections. So far, this includes a single line 
+  // Render data for all other sections. So far, this includes a single line
   // chart showing requests over time, and a table for requesting hosts
   else {
     var sectionInfo = {
@@ -255,12 +208,12 @@ function processOverlay(evt) {
 }
 
 /**
- * Removes the overlay element from the document body, restoring the previous 
+ * Removes the overlay element from the document body, restoring the previous
  * screen. Also re-enables the page's scrollbar.
  *
  * @param  evt  The click event that triggered the listener
  */
-function removeOverlay(evt) {
+ function removeOverlay(evt) {
   $('body').css('overflow', 'visible');
   $('#overlay').remove();
 
@@ -269,11 +222,3 @@ function removeOverlay(evt) {
 
 // Register the table partial
 Handlebars.registerPartial('table', $('#table-partial').html());
-
-// Add the listeners
-var uploadBox = $('#uploadbox');
-uploadBox.on('dragover', handleDragOver);
-uploadBox.on('drop', handleFileSelect);
-
-$('#showfileinput').on('click', showFileInput);
-$('#fileinput').on('change', handleFileSelect);
